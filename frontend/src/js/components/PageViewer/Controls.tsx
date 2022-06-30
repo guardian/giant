c3b09c82ce70de9f151ea2397d83b15b8855d773
Joseph Smith <joseph.lochlann@gmail.com>
2022-06-30 13:10:21 +0100

    Review comments

--- frontend/src/js/components/PageViewer/Controls.tsx
+++ frontend/src/js/components/PageViewer/Controls.tsx
@@ -60,7 +60,7 @@ export const Controls: FC<ControlsProps> = ({
       .then((highlights) => {
         setIsFindPending(false);
         setFindHighlights(highlights);
-        if (highlights.length) {
+        if (highlights.length > 0) {
           setFocusedFindHighlightIndex(0);
         } else {
           setFocusedFindHighlightIndex(null);
@@ -90,6 +90,7 @@ export const Controls: FC<ControlsProps> = ({
   }, [findHighlights, focusedFindHighlightIndex, setFocusedFindHighlightIndex]);
 
   const handleUserKeyPress = useCallback((e) => {
+    // Cmd + F
     if ((e.ctrlKey || e.metaKey) && e.keyCode === 70) {
       e.preventDefault();
       setFindVisible(true);
