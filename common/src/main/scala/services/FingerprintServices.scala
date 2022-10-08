07051512b5212467dd4eadf1b936b9f9e62825dc
Michael Barton <michael@mbarton.cc>
2022-10-08 18:36:23 +0100

    Remove Guava from common

--- common/src/main/scala/services/FingerprintServices.scala
+++ common/src/main/scala/services/FingerprintServices.scala
@@ -1,23 +1,37 @@
 package services
 
-import java.io.File
-import java.nio.file.Files
-import java.util.{Base64, UUID}
+import org.apache.commons.codec.digest.DigestUtils
 
-import com.google.common.hash.{HashFunction, Hashing}
-import com.google.common.io.{Files => GuavaFiles}
+import java.io.{File, InputStream, OutputStream}
+import java.nio.file.Files
+import java.security.{DigestInputStream, MessageDigest}
+import java.util.{Base64, UUID}
 
 object FingerprintServices {
   val DEFAULT_HASH: String = "SHA-512"
-  val DEFAULT_HASH_FUNCTION: HashFunction = Hashing.sha512()
+
   private val encoder = Base64.getUrlEncoder.withoutPadding
 
   def createFingerprintFromFile(file: File): String = {
     if(Files.size(file.toPath) == 0) {
       s"urn:pfi:giant:zero-byte-file:${UUID.randomUUID()}"
     } else {
-      val bytes = GuavaFiles.asByteSource(file).hash(DEFAULT_HASH_FUNCTION).asBytes()
-      encoder.encodeToString(bytes)
+      var inputStream: InputStream = null
+
+      try {
+        inputStream = Files.newInputStream(file.toPath)
+
+        val digest = MessageDigest.getInstance(DEFAULT_HASH)
+        val digestInputStream = new DigestInputStream(inputStream, digest)
+
+        digestInputStream.transferTo(OutputStream.nullOutputStream())
+
+        val bytes = digest.digest()
+
+        encoder.encodeToString(bytes)
+      } finally {
+        Option(inputStream).foreach(_.close())
+      }
     }
   }
 }
