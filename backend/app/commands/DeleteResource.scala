b0d68dfc7aed8123d58524745380a5ef003d3fca
Michael Barton <michael@mbarton.cc>
2022-10-08 20:19:22 +0100

    [error] /home/michael/giant/backend/app/commands/DeleteResource.scala:70:10: `Unit` companion object is not allowed in source; instead, use `()` for the unit value [error]          Unit [error]          ^

--- backend/app/commands/DeleteResource.scala
+++ backend/app/commands/DeleteResource.scala
@@ -67,7 +67,7 @@ class DeleteResource( manifest: Manifest, index: Index, previewStorage: ObjectSt
          _ <- manifest.deleteBlob(uri)
          _ <- successAttempt
        } yield {
-         Unit
+         ()
        }
      }
 
