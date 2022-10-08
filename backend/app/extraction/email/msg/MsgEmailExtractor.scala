e233a4ca51a31eb2e27b02b9dbfaefe8871340d1
Michael Barton <michael@mbarton.cc>
2022-10-08 20:21:58 +0100

    [error] /home/michael/giant/backend/app/extraction/email/msg/MsgEmailExtractor.scala:106:11: `Unit` companion object is not allowed in source; instead, use `()` for the unit value [error]     Right(Unit) [error]           ^

--- backend/app/extraction/email/msg/MsgEmailExtractor.scala
+++ backend/app/extraction/email/msg/MsgEmailExtractor.scala
@@ -103,6 +103,6 @@ class MsgEmailExtractor(scratch: ScratchSpace, ingestionServices: IngestionServi
 
   override def extract(blob: Blob, stream: InputStream, params: ExtractionParams): Either[Failure, Unit] = {
     processMessage(blob, new OutlookMessageParser().parseMsg(stream), params)
-    Right(Unit)
+    Right(())
   }
 }
