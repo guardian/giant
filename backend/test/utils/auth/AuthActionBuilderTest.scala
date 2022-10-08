82d206fca3bef6b82ae32adbd254d7a00dd1026c
Michael Barton <michael@mbarton.cc>
2022-10-08 21:13:22 +0100

    [error] /home/michael/giant/backend/test/utils/auth/AuthActionBuilderTest.scala:48:47: Auto-application to `()` is deprecated. Supply the empty argument list `()` explicitly to invoke method fakeApplication, [error] or remove the empty argument list from its definition (Java-defined methods are exempt). [error] In Scala 3, an unapplied method like this will be eta-expanded into a function. [error]   implicit val configuration: Configuration = fakeApplication.configuration

--- backend/test/utils/auth/AuthActionBuilderTest.scala
+++ backend/test/utils/auth/AuthActionBuilderTest.scala
@@ -30,7 +30,7 @@ import org.scalatest.matchers.should.Matchers
 class AuthActionBuilderTest extends AnyFreeSpec with Matchers with BaseOneAppPerSuite with FakeApplicationFactory
   with EitherValues with Results with Inside {
 
-  override def fakeApplication: Application = {
+  override def fakeApplication(): Application = {
     val env = Environment.simple(new File("."))
     val initialSettings: Map[String, AnyRef] = Map(
       "play.http.secret.key" -> "TestKey",
@@ -45,13 +45,13 @@ class AuthActionBuilderTest extends AnyFreeSpec with Matchers with BaseOneAppPer
   }
 
   // To appease jwt-scala
-  implicit val configuration: Configuration = fakeApplication.configuration
+  implicit val configuration: Configuration = fakeApplication().configuration
   implicit val clock: Clock = Clock.systemUTC()
 
   val fakeUsers = TestUserManagement(List(user.DBUser("mickey", Some("Mickey Mouse"), Some(BCryptPassword("invalid")), None, registered = true, None)))
 
   "AuthActionBuilder" - {
-    val authActionBuilder = new DefaultAuthActionBuilder(Helpers.stubControllerComponents(), new DefaultFailureToResultMapper, 8 hours, 15 minutes, fakeUsers)(fakeApplication.configuration, Clock.systemUTC())
+    val authActionBuilder = new DefaultAuthActionBuilder(Helpers.stubControllerComponents(), new DefaultFailureToResultMapper, 8 hours, 15 minutes, fakeUsers)(fakeApplication().configuration, Clock.systemUTC())
     val userMickey = User("mickey", "Mickey Mouse")
     val now = new DateTime(2017, 1, 5, 16, 0, 0)
 
