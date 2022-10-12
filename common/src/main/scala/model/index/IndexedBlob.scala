39d19f4f52c900b03ea813784f0957719631dce6
Joseph Smith <joseph.lochlann@gmail.com>
2022-10-12 17:36:36 +0100

    inMultiple flag to get only those blobs that are in multiple collection/ingestions

--- common/src/main/scala/model/index/IndexedBlob.scala
+++ common/src/main/scala/model/index/IndexedBlob.scala
@@ -2,7 +2,7 @@ package model.index
 
 import play.api.libs.json.{Format, Json}
 
-case class IndexedBlob(uri: String, ingestion: Set[String])
+case class IndexedBlob(uri: String, collection: Set[String], ingestion: Set[String])
 object IndexedBlob {
   implicit val format: Format[IndexedBlob] = Json.format[IndexedBlob]
 }
