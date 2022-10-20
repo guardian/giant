10716b851b3828a6675dfba93f3bcb17e1c2514e
Joseph Smith <joseph.lochlann@gmail.com>
2022-10-20 15:08:24 +0100

    Add timing trait

---
+++ common/src/main/scala/utils/Timing.scala
@@ -0,0 +1,35 @@
+package utils
+
+import utils.attempt.Attempt
+
+import scala.concurrent.ExecutionContext
+
+trait Timing extends Logging {
+  private def log(name: String, totalElapsedNanoseconds: Long): Unit = {
+    // TODO: use Duration instead?
+    val elapsedNanoseconds = (totalElapsedNanoseconds % 1000000).toInt
+    val elapsedMilliseconds =(totalElapsedNanoseconds / 1000000).toInt
+    val elapsedSeconds = (elapsedMilliseconds / 1000).toInt
+    val elapsedMinutes = (elapsedSeconds / 60).toInt
+    logger.info(s"${name}: took ${elapsedMinutes} minutes ${elapsedSeconds} seconds ${elapsedMilliseconds} milliseconds ${elapsedNanoseconds} nanoseconds")
+  }
+  def timeAsync[T](name: String, attempt: Attempt[T])(implicit ec: ExecutionContext): Attempt[T] = {
+    val start = System.nanoTime()
+
+    attempt.map { result =>
+      val end = System.nanoTime()
+      log(name, end - start)
+      result
+    }
+  }
+
+  def timeSync[T](name: String, block: => T): T = {
+    val start = System.nanoTime()
+    val res = block
+    val end = System.nanoTime()
+
+    log(name, end - start)
+
+    res
+  }
+}
