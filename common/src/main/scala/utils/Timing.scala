40bbd17ec7189a6e05fc9aab05ac2b589ad54980
Joseph Smith <joseph.lochlann@gmail.com>
2022-10-21 11:36:31 +0100

    Just up to milliseconds

--- common/src/main/scala/utils/Timing.scala
+++ common/src/main/scala/utils/Timing.scala
@@ -5,28 +5,26 @@ import utils.attempt.Attempt
 import scala.concurrent.ExecutionContext
 
 trait Timing extends Logging {
-  private def log(name: String, totalElapsedNanoseconds: Long): Unit = {
-    // TODO: use Duration instead?
-    val elapsedNanoseconds = (totalElapsedNanoseconds % 1000000).toInt
-    val elapsedMilliseconds =(totalElapsedNanoseconds / 1000000).toInt
-    val elapsedSeconds = (elapsedMilliseconds / 1000).toInt
-    val elapsedMinutes = (elapsedSeconds / 60).toInt
-    logger.info(s"${name}: took ${elapsedMinutes} minutes ${elapsedSeconds} seconds ${elapsedMilliseconds} milliseconds ${elapsedNanoseconds} nanoseconds")
+  private def log(name: String, totalElapsedMilliseconds: Long): Unit = {
+    val elapsedMilliseconds =(totalElapsedMilliseconds % 1000).toInt
+    val elapsedSeconds = (totalElapsedMilliseconds / 1000).toInt
+    val elapsedMinutes = elapsedSeconds / 60
+    logger.info(s"${name}: took ${elapsedMinutes} minutes ${elapsedSeconds} seconds ${elapsedMilliseconds} milliseconds")
   }
   def timeAsync[T](name: String, attempt: => Attempt[T])(implicit ec: ExecutionContext): Attempt[T] = {
-    val start = System.nanoTime()
+    val start = System.currentTimeMillis()
 
     attempt.map { result =>
-      val end = System.nanoTime()
+      val end = System.currentTimeMillis()
       log(name, end - start)
       result
     }
   }
 
   def timeSync[T](name: String, block: => T): T = {
-    val start = System.nanoTime()
+    val start = System.currentTimeMillis()
     val res = block
-    val end = System.nanoTime()
+    val end = System.currentTimeMillis()
 
     log(name, end - start)
 
