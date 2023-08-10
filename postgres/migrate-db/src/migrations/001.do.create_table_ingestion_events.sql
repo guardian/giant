ccf03c70aeb346721a197903490c6fa4b3528d32
Zeke Hunter-Green <zeke.huntergreen@guardian.co.uk>
2023-08-10 15:27:49 +0100

    add status to ingestion events, add blob_metadata table

--- postgres/migrate-db/src/migrations/001.do.create_table_ingestion_events.sql
+++ postgres/migrate-db/src/migrations/001.do.create_table_ingestion_events.sql
@@ -2,6 +2,12 @@ CREATE TABLE ingestion_events (
 	blob_id TEXT,
 	ingest_uri TEXT,
 	"type" TEXT NOT NULL,
+	status TEXT NOT NULL,
 	details JSONB,
 	created_at TIMESTAMP NOT NULL
+);
+CREATE TABLE blob_metadata (
+	blob_id TEXT PRIMARY KEY,
+	file_name TEXT,
+	path TEXT
 );
