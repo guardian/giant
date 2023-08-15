9964464adf97db860ad59bb173bf173601267ce0
Zeke Hunter-Green <zeke.huntergreen@guardian.co.uk>
2023-08-15 11:57:28 +0100

    update ingestion events and metadata schemas
    Co-Authored-By: Marjan Kalanaki <15894063+marjisound@users.noreply.github.com>
    Co-Authored-By: Philip McMahon <philip.mcmahon@guardian.co.uk>

--- postgres/migrate-db/src/migrations/001.do.create_table_ingestion_events.sql
+++ postgres/migrate-db/src/migrations/001.do.create_table_ingestion_events.sql
@@ -1,14 +1,21 @@
 CREATE TABLE ingestion_events (
-	blob_id TEXT,
-	ingest_id TEXT,
+	ingest_id TEXT NOT NULL,
+	blob_id TEXT NOT NULL,
 	"type" TEXT NOT NULL,
 	status TEXT NOT NULL,
 	details JSONB,
-    event_time TIMESTAMP NOT NULL
+	event_time TIMESTAMP NOT NULL
 );
 CREATE TABLE blob_metadata (
-	blob_id TEXT PRIMARY KEY,
-	file_name TEXT,
-	file_size INT,
-	path TEXT
+	ingest_id TEXT NOT NULL,
+	blob_id TEXT NOT NULL,
+	path TEXT NOT NULL, -- includes file name
+	file_size BIGINT NOT NULL,
+	insert_time TIMESTAMP NOT NULL
 );
+CREATE INDEX ON ingestion_events (ingest_uri, blob_id);
+CREATE INDEX ON blob_metadata (ingest_uri, blob_id);
+CREATE INDEX ON ingestion_events ("type");
+CREATE INDEX ON ingestion_events ((details->>'extractorName'));
+-- TODO think about indexing details->>'extractors'
+-- TODO think about indexing workspace
