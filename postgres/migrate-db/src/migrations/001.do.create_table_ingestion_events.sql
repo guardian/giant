1f8eaf184a6a6e70ff08c30a54e402b1ffef0bfd
Marjan Kalanaki <marjan.kalanaki@guardian.co.uk>
2023-08-22 09:05:14 +0100

    use timestamptz for date fields in db

--- postgres/migrate-db/src/migrations/001.do.create_table_ingestion_events.sql
+++ postgres/migrate-db/src/migrations/001.do.create_table_ingestion_events.sql
@@ -4,18 +4,17 @@ CREATE TABLE ingestion_events (
 	"type" TEXT NOT NULL,
 	status TEXT NOT NULL,
 	details JSONB,
-	event_time TIMESTAMP NOT NULL
+	event_time TIMESTAMPTZ NOT NULL
 );
 CREATE TABLE blob_metadata (
 	ingest_id TEXT NOT NULL,
 	blob_id TEXT NOT NULL,
 	path TEXT NOT NULL, -- includes file name
 	file_size BIGINT NOT NULL,
-	insert_time TIMESTAMP NOT NULL
+	insert_time TIMESTAMPTZ NOT NULL
 );
 CREATE INDEX ON ingestion_events (ingest_id, blob_id);
 CREATE INDEX ON blob_metadata (ingest_id, blob_id);
 CREATE INDEX ON ingestion_events ("type");
-CREATE INDEX ON ingestion_events ((details->>'extractorName'));
 -- TODO think about indexing details->>'extractors'
 -- TODO think about indexing workspace
