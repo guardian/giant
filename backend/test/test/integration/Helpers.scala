e2b6750f732c37871e79c9ebe9e542cbb91158f0
Marjan Kalanaki <marjan.kalanaki@guardian.co.uk>
2023-09-20 09:23:06 +0100

    add test for deleting item in a workspace

--- backend/test/test/integration/Helpers.scala
+++ backend/test/test/integration/Helpers.scala
@@ -129,6 +129,11 @@ object Helpers extends Matchers with Logging with OptionValues with Inside {
       .apply(FakeRequest())
   }
 
+  def deleteFileFromWorkspace(workspaceId: String, blobUri: String)(implicit controllers: Controllers, timeout: Timeout): Future[Result] = {
+    controllers.workspace.deleteBlob(workspaceId, blobUri)
+      .apply(FakeRequest())
+  }
+
   def removeFileFromWorkspaceAssertingSuccess(workspaceId: String, itemId: String)(implicit controllers: Controllers, timeout: Timeout): Assertion = {
     val responseCode = status(removeFileFromWorkspace(workspaceId, itemId))
     responseCode should be(204)
@@ -251,7 +256,7 @@ object Helpers extends Matchers with Logging with OptionValues with Inside {
       val collectionsController = new Collections(controllerComponents, manifest, userManagement, elasticsearch.elasticResources, s3Config, elasticsearch.elasticEvents, elasticsearch.elasticPages, ingestionServices, annotations)
       val resourceController = new Resource(controllerComponents, manifest, elasticsearch.elasticResources, elasticsearch.elasticPages,  annotations, null)
       val filtersController = new Filters(controllerComponents, manifest, annotations)
-      val workspaceController = new Workspaces(controllerComponents, annotations, elasticsearch.elasticResources, manifest, userManagement, null, null)
+      val workspaceController = new Workspaces(controllerComponents, annotations, elasticsearch.elasticResources, manifest, userManagement, new TestObjectStorage(), new TestObjectStorage())
       val searchController = new Search(controllerComponents, userManagement, elasticsearch.elasticResources, annotations)
       val documentsController = new Documents(controllerComponents, manifest, elasticsearch.elasticResources, null, userManagement, annotations, downloadExpiryPeriod)
       val previewsController = new Previews(controllerComponents, manifest, elasticsearch.elasticResources, new TestPreviewService, userManagement, annotations, downloadExpiryPeriod)
