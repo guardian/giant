827c8cd8ae9a4ab8d534fa8f9a1c101f8b115950
Zeke Hunter-Green <zeke.huntergreen@guardian.co.uk>
2023-10-25 16:25:45 +0100

    compile whisper in setup script

--- scripts/setup_whisper.sh
+++ scripts/setup_whisper.sh
@@ -5,3 +5,8 @@ git clone git@github.com:ggerganov/whisper.cpp.git ~/code/whisper.cpp
 
 echo "downloading ggml model"
 bash ~/code/whisper.cpp/models/download-ggml-model.sh large
+
+echo "compiling whisper.cpp"
+pushd ~/code/whisper.cpp
+make
+popd
