c79f5b6e7aa6158666ca9aefc051102c3548c53c
Zeke Hunter-Green <zeke.huntergreen@guardian.co.uk>
2024-03-20 17:01:58 +0000

    Revert "change log"
    This reverts commit b1eb24013cd176a69d10833fe04925e7a2cf2584.

--- backend/app/controllers/api/Search.scala
+++ backend/app/controllers/api/Search.scala
@@ -23,10 +23,7 @@ class Search(override val controllerComponents: AuthControllerComponents, userMa
   def search() = ApiAction.attempt { req: UserIdentityRequest[_] =>
     val q = req.queryString.getOrElse("q", Seq("")).head
     val proposedParams = Search.buildSearchParameters(q, req)
-    logger.info(s"searching. workspace context is ${proposedParams.workspaceContext.isDefined}")
-    if (proposedParams.workspaceContext.isDefined){
-      logger.info(req.user.asLogMarker, "Performing workspace search")
-    }
+    proposedParams.workspaceContext.map(wc => logger.info(req.user.asLogMarker, s"Performing workspace search"))
 
     buildSearch(req.user, proposedParams, proposedParams.workspaceContext).flatMap { case (verifiedParams, context) =>
       val returnEmptyResult = Search.shouldReturnEmptyResult(proposedParams, verifiedParams, context)
