cdd5215f87ea8a922fd8b9dca2ad4bd69cff56cb
Zeke Hunter-Green <zeke.huntergreen@guardian.co.uk>
2024-03-21 10:03:28 +0000

    Add workspace search log (#205)
    * log when a user performs a workspace search

--- backend/app/controllers/api/Search.scala
+++ backend/app/controllers/api/Search.scala
@@ -23,6 +23,7 @@ class Search(override val controllerComponents: AuthControllerComponents, userMa
   def search() = ApiAction.attempt { req: UserIdentityRequest[_] =>
     val q = req.queryString.getOrElse("q", Seq("")).head
     val proposedParams = Search.buildSearchParameters(q, req)
+    proposedParams.workspaceContext.map(wc => logger.info(req.user.asLogMarker, s"Performing workspace search"))
 
     buildSearch(req.user, proposedParams, proposedParams.workspaceContext).flatMap { case (verifiedParams, context) =>
       val returnEmptyResult = Search.shouldReturnEmptyResult(proposedParams, verifiedParams, context)
