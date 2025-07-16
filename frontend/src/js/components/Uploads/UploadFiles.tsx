ee6ce182c16da6bd18a871761bdfca7b548bc76c
Philip McMahon <philip.mcmahon@guardian.co.uk>
2025-07-16 12:44:14 +0100

    Bump max upload size via ui to 300mb

--- frontend/src/js/components/Uploads/UploadFiles.tsx
+++ frontend/src/js/components/Uploads/UploadFiles.tsx
@@ -20,7 +20,7 @@ import { getWorkspace } from '../../actions/workspaces/getWorkspace';
 import { useDispatch } from 'react-redux';
 import { AppActionType } from '../../types/redux/GiantActions';
 
-const MAX_FILE_UPLOAD_SIZE_MBYTES = 250 // should correspond to http.parser.maxDiskBuffer in application.conf
+const MAX_FILE_UPLOAD_SIZE_MBYTES = 300 // should correspond to http.parser.maxDiskBuffer in application.conf
 const MAX_FILE_UPLOAD_SIZE_BYTES = MAX_FILE_UPLOAD_SIZE_MBYTES*1024*1024
 
 
