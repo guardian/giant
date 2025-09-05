9a4693de8d466f74fcd7131bb7c6e6298e6f6db9
Tom Richards <tom.richards@guardian.co.uk>
2025-09-05 22:15:42 +0100

    Add new `CaptureFromUrl` modal with button to open (like `UploadFiles`) but also detects the `capture_from_url` query param. Also used (without the button) on the `/workspaces` (i.e. when no workspace selected)

---
+++ frontend/src/js/components/Uploads/CaptureFromUrl.tsx
@@ -0,0 +1,197 @@
+import MdGlobeIcon from "react-icons/lib/md/public";
+
+import React, {useEffect, useMemo, useState} from "react";
+import {Workspace, WorkspaceEntry, WorkspaceMetadata} from "../../types/Workspaces";
+import Modal from "../UtilComponents/Modal";
+import Select from "react-select";
+import TreeBrowser from "../UtilComponents/TreeBrowser";
+import {connect} from "react-redux";
+import {GiantState} from "../../types/redux/GiantState";
+import {isTreeLeaf, isTreeNode, TreeEntry, TreeNode} from "../../types/Tree";
+import {GiantDispatch} from "../../types/redux/GiantDispatch";
+import {bindActionCreators} from "redux";
+import {getWorkspace} from "../../actions/workspaces/getWorkspace";
+import {setNodeAsExpanded} from "../../actions/workspaces/setNodeAsExpanded";
+import {setNodeAsCollapsed} from "../../actions/workspaces/setNodeAsCollapsed";
+import {displayRelativePath} from "../../util/workspaceUtils";
+import {getWorkspacesMetadata} from "../../actions/workspaces/getWorkspacesMetadata";
+import {setFocusedEntry} from "../../actions/workspaces/setFocusedEntry";
+
+export const getMaybeCaptureFromUrlQueryParamValue = () =>
+  new URLSearchParams(window.location.search).get("capture_from_url");
+
+interface CaptureFromUrlProps {
+  withButton?: true,
+  maybePreSelectedWorkspace?: Workspace,
+  getWorkspacesMetadata: () => void,
+  workspacesMetadata: WorkspaceMetadata[],
+  currentWorkspace: Workspace | null,
+  setCurrentWorkspace: (id: string) => void,
+  expandedNodes: TreeNode<WorkspaceEntry>[],
+  setNodeAsExpanded: (node: TreeNode<WorkspaceEntry>) => void,
+  setNodeAsCollapsed: (node: TreeNode<WorkspaceEntry>) => void,
+  focusedEntry: TreeEntry<WorkspaceEntry> | null,
+  setFocusedEntry: (entry: TreeEntry<WorkspaceEntry> | null) => void,
+}
+
+export const CaptureFromUrl = connect(
+  (state: GiantState) => ({
+    workspacesMetadata: state.workspaces.workspacesMetadata,
+    currentWorkspace: state.workspaces.currentWorkspace,
+    expandedNodes: state.workspaces.expandedNodes,
+    focusedEntry: state.workspaces.focusedEntry,
+  }),
+  (dispatch: GiantDispatch) => ({
+    getWorkspacesMetadata: bindActionCreators(getWorkspacesMetadata, dispatch),
+    setCurrentWorkspace: bindActionCreators(getWorkspace, dispatch), // aliased to setCurrentWorkspace because that's what it does
+    setNodeAsExpanded: bindActionCreators(setNodeAsExpanded, dispatch),
+    setNodeAsCollapsed: bindActionCreators(setNodeAsCollapsed, dispatch),
+    setFocusedEntry: bindActionCreators(setFocusedEntry, dispatch),
+  })
+)(({
+     withButton,
+     workspacesMetadata,
+     currentWorkspace,
+     setCurrentWorkspace,
+     maybePreSelectedWorkspace,
+     expandedNodes,
+     setNodeAsExpanded,
+     setNodeAsCollapsed,
+     focusedEntry,
+     setFocusedEntry,
+   }: CaptureFromUrlProps) => {
+
+  const [isOpen, setIsOpen] = useState(false);
+  useEffect(() => {getWorkspacesMetadata()}, [isOpen]);
+
+  const maybeCaptureFromUrlViaQueryParamValue = useMemo(
+    getMaybeCaptureFromUrlQueryParamValue,
+    []
+  );
+
+  const [url, setUrl] = useState<string>(maybeCaptureFromUrlViaQueryParamValue ?? "");
+
+  const workspace = currentWorkspace || maybePreSelectedWorkspace;
+
+  const [saveAs, setSaveAs] = useState<string>("");
+
+  useEffect(() => {
+    if (maybeCaptureFromUrlViaQueryParamValue) {
+      setIsOpen(true);
+    }
+  }, [maybeCaptureFromUrlViaQueryParamValue]);
+
+  const onSubmit = () => {
+    setIsOpen(false);
+  }
+
+  return <>
+    {withButton &&
+      <button
+        className='btn file-upload__button'
+        onClick={() => setIsOpen(true)}
+      >
+        <MdGlobeIcon className='file-upload__icon'/>
+        Capture from URL
+      </button>
+    }
+    <Modal isOpen={isOpen} dismiss={() => setIsOpen(false)}>
+      <form className='form' onSubmit={onSubmit}>
+        <h2 className='modal__title'>Capture from URL</h2>
+
+        <div className='form__row'>
+          <span className='form__label required-field'>URL</span>
+          <input type="text"
+                 autoFocus={!maybeCaptureFromUrlViaQueryParamValue}
+                 value={url}
+                 onChange={e => setUrl(e.target.value)}/>
+        </div>
+
+        <div className='form__row'>
+          <span className='form__label required-field'>Workspace</span>
+          <Select
+            name='workspace-select'
+            autoFocus
+            options={workspacesMetadata.map(w => ({
+              value: w.id,
+              label: w.name
+            }))}
+            searchable
+            clearable
+            cle
+            isMulti={false}
+            value={workspace ? {
+              label: workspace.name,
+              value: workspace.id
+            } : null}
+            onChange={selected => selected && setCurrentWorkspace(selected.value)}
+          />
+        </div>
+
+        <div className='form__row'>
+          <span className='form__label'>Folder</span>
+          {
+            currentWorkspace
+              ?
+              <div className='workspace__tree workspace__tree--fixed'>
+                <TreeBrowser
+                  showColumnHeaders={false}
+                  rootId={currentWorkspace.rootNode.id}
+                  tree={currentWorkspace.rootNode.children}
+                  onFocus={entry => isTreeNode(entry) && setFocusedEntry(entry)}
+                  clearFocus={() => setFocusedEntry(null)}
+                  selectedEntries={focusedEntry ? [focusedEntry] : []}
+                  focusedEntry={focusedEntry}
+                  onMoveItems={() => {}} // can't move things in this view
+                  onContextMenu={() => {}} // can't rename things in this view
+                  onSelectLeaf={() => {}} // leaves are not valid targets to add to, so are not selectable
+                  onExpandLeaf={() => {}} // entire tree is in memory up-front, so no need for an expand leaf callback
+                  onClickColumn={() => {}} // only one column, don't allow reversing the sorting
+                  columnsConfig={{
+                    columns: [{
+                      name: 'Name',
+                      align: 'left',
+                      render: (entry) =>
+                        isTreeNode(entry)
+                          ? <>{entry.name || '--'} <span style={{marginLeft: "5px", fontSize: "smaller", color: "#666"}}>
+                            ({entry.children.filter(isTreeLeaf).length} files & {entry.children.filter(isTreeNode).length} folders)
+                          </span></>
+                          : <></>, // don't render leaves since they can't be selected
+                      sort: (a, b) => a.name.localeCompare(b.name),
+                      style: {},
+                    }],
+                    sortDescending: false,
+                    sortColumn: 'Name',
+                  }}
+                  expandedEntries={expandedNodes}
+                  onExpandNode={setNodeAsExpanded}
+                  onCollapseNode={setNodeAsCollapsed}
+                />
+              </div>
+              :
+              <div className='workspace__tree workspace__tree--fixed'>
+                <p className='centered diminish'>Select a workspace...</p>
+              </div>
+          }
+        </div>
+
+        <div className='form__row'>
+          <span className='form__label required-field'>New folder name</span>
+          <em>Capturing a URL into Giant will capture as much as possible from the page,
+            so a new folder is required to keep everything together.</em>
+          <input type='text' value={saveAs} onChange={e => setSaveAs(e.target.value)}/>
+        </div>
+
+        <div className='form__row'>
+          <button type='submit' className='btn' disabled={!currentWorkspace || !saveAs}>
+            Save
+          </button>
+          {currentWorkspace && saveAs && (<>
+          {" "} to <code>{`${displayRelativePath(currentWorkspace.rootNode, focusedEntry?.id || currentWorkspace.rootNode.id)}${saveAs}`}</code>
+          </>)}
+        </div>
+
+      </form>
+    </Modal>
+  </>
+});
