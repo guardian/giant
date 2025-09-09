63e93562dd6aff4ffe8a087b8def933fb31ed22a
Tom Richards <tom.richards@guardian.co.uk>
2025-09-09 17:03:27 +0100

    actually ingest the resulting file(s) from remote ingest job
    Co-authored-by: Philip McMahon <philip.mcmahon@guardian.co.uk>

--- backend/app/controllers/api/Workspaces.scala
+++ backend/app/controllers/api/Workspaces.scala
@@ -50,7 +50,7 @@ object AddItemParameters {
   implicit val format = Json.format[AddItemParameters]
 }
 
-case class AddRemoteUrlData(url: String, title: String, parentFolderId: String, workspaceNodeId: String)
+case class AddRemoteUrlData(url: String, title: String, parentFolderId: String)
 object AddRemoteUrlData {
   implicit val format = Json.format[AddRemoteUrlData]
 }
@@ -278,7 +278,7 @@ class Workspaces(
         title = data.title,
         url = data.url,
         parentFolderId = data.parentFolderId,
-        timeoutAt = DateTime.now.plus(Duration.standardHours(4)), // 4 hours
+        createdAt = DateTime.now,
         status = "started",
         userEmail = req.user.username
       )
