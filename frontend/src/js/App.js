cb9419c8f38f330754b54fe06f7564e46b6e75d1
S Dimmock <srbd@users.noreply.github.com>
2025-11-12 18:39:47 +0000

    Revert "Add route for file and folder locations within workspaces"

--- frontend/src/js/App.js
+++ frontend/src/js/App.js
@@ -91,8 +91,6 @@ class App extends React.Component {
                     <Route path='/settings/features' component={FeatureSwitches} />
                     <Route path='/settings/about' component={About} />
                     <Route path='/settings/uploads' component={WeeklyUploadsFeed} />
-                    <Route path='/workspaces/:id'  component={Workspaces} />
-                    <Route path='/workspaces/:id/:workspaceLocation'  component={Workspaces} />
                     <Route path='/workspaces/:id' component={Workspaces} />
                     <Route exact path='/workspaces' component={CaptureFromUrl} />
 
