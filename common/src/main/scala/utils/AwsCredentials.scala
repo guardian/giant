dd4552b1b8eb8aaa4f9a5933335f9b5f0b92dc3d
S Dimmock <sabina.bejasa-dimmock@theguardian.com>
2025-12-12 12:23:06 +0000

    first pass upgrading ec2 to aws sdk v2

--- common/src/main/scala/utils/AwsCredentials.scala
+++ common/src/main/scala/utils/AwsCredentials.scala
@@ -1,27 +1,52 @@
 package utils
 
-import com.amazonaws.auth.profile.ProfileCredentialsProvider
-import com.amazonaws.auth._
+import com.amazonaws.auth.profile.{ProfileCredentialsProvider => ProfileCredentialsProviderV1}
+import com.amazonaws.auth.{AWSCredentialsProviderChain => AWSCredentialsProviderChainV1}
+import com.amazonaws.auth.{AWSStaticCredentialsProvider => AWSStaticCredentialsProviderV1}
+import com.amazonaws.auth.{BasicAWSCredentials => BasicAWSCredentialsV1}
+import com.amazonaws.auth.{AWSCredentialsProvider => AWSCredentialsProviderV1}
+import com.amazonaws.auth.{InstanceProfileCredentialsProvider => InstanceProfileCredentialsProviderV1}
+import software.amazon.awssdk.auth.credentials.{AwsBasicCredentials, AwsCredentialsProvider, AwsCredentialsProviderChain, InstanceProfileCredentialsProvider, ProfileCredentialsProvider, StaticCredentialsProvider}
+import scala.jdk.CollectionConverters._
 
 object AwsCredentials {
-  def apply(accessKey: Option[String] = None, secretKey: Option[String] = None, profile: Option[String] = None): AWSCredentialsProvider = {
+  def apply(accessKey: Option[String] = None, secretKey: Option[String] = None, profile: Option[String] = None): AWSCredentialsProviderV1 = {
     // You must never break the chain...
     // Static credentials should come first in order to override any federated credentials a dev may have on their machine
-    new AWSCredentialsProviderChain(minioCredentials(accessKey, secretKey) ++ awsCredentials(profile) : _*)
+    new AWSCredentialsProviderChainV1(minioCredentials(accessKey, secretKey) ++ awsCredentials(profile) : _*)
   }
 
-  private def awsCredentials(profile: Option[String]): List[AWSCredentialsProvider] = {
+  def credentialsV2(accessKey: Option[String] = None, secretKey: Option[String] = None, profile: Option[String] = None): AwsCredentialsProvider = {
+    val credentialsProviders = minioCredentialsV2(accessKey, secretKey) ++ awsCredentialsV2(profile)
+    AwsCredentialsProviderChain.builder()
+      .credentialsProviders(credentialsProviders.asJava)
+      .build()
+  }
+
+  private def awsCredentials(profile: Option[String]): List[AWSCredentialsProviderV1] = {
     List(
-      new ProfileCredentialsProvider(profile.getOrElse("investigations")),
-      InstanceProfileCredentialsProvider.getInstance()
+      new ProfileCredentialsProviderV1(profile.getOrElse("investigations")),
+      InstanceProfileCredentialsProviderV1.getInstance()
     )
   }
 
-  private def minioCredentials(accessKey: Option[String], secretKey: Option[String]): List[AWSCredentialsProvider] = {
+  private def awsCredentialsV2(profile: Option[String]): List[AwsCredentialsProvider] = {
+    List(ProfileCredentialsProvider.create(profile.getOrElse("investigations")))
+  }
+
+  private def minioCredentials(accessKey: Option[String], secretKey: Option[String]): List[AWSCredentialsProviderV1] = {
     (accessKey, secretKey) match {
       case (Some(ak), Some(sk)) =>
-        List(new AWSStaticCredentialsProvider(new BasicAWSCredentials(ak, sk)))
+        List(new AWSStaticCredentialsProviderV1(new BasicAWSCredentialsV1(ak, sk)))
+      case _ =>
+        List.empty
+    }
+  }
 
+  private def minioCredentialsV2(accessKey: Option[String], secretKey: Option[String]): List[AwsCredentialsProvider] = {
+    (accessKey, secretKey) match {
+      case (Some(ak), Some(sk)) =>
+        List(StaticCredentialsProvider.create(AwsBasicCredentials.create(ak, sk)))
       case _ =>
         List.empty
     }
