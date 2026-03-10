a5d10d0f54f29b557c4e9d86e29bed197bd7924b
Tom Richards <tom.richards@guardian.co.uk>
2026-03-10 11:40:44 +0000

    [infra] add empty neo4j stack ready to start migrating neo4j

--- infra/cdk/bin/cdk.ts
+++ infra/cdk/bin/cdk.ts
@@ -1,5 +1,6 @@
 import {RiffRaffYamlFile} from "@guardian/cdk/lib/riff-raff-yaml-file";
 import {App} from "aws-cdk-lib";
+import { Neo4j } from '../lib/neo4j';
 import { Postgres } from '../lib/postgres';
 
 const app = new App();
@@ -23,9 +24,21 @@ export const guStacks = [
 		app: 'postgres',
 	}),
 
+  new Neo4j(app, 'pfi-giant-neo4j-CODE', {
+    env,
+    stack,
+    stage: 'CODE',
+    app: 'neo4j',
+  }),
+  new Neo4j(app, 'pfi-giant-neo4j-PROD', {
+    env,
+    stack,
+    stage: 'PROD',
+    app: 'neo4j',
+  }),
+
 ];
 
 // synthing the riff-raff.yaml explicitly (rather than replacing 'new App()' with GuRoot) so we can alter deployments
 export const riffRaff = new RiffRaffYamlFile(app);
 riffRaff.synth();
-
