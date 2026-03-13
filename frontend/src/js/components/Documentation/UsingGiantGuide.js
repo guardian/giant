d9bb86e25ddbd58606696253f50d51f44aeacb62
Luke Hoyland <luke.hoyland@theguardian.com>
2026-03-13 18:55:06 +0000

    Remove stale .js files superseded by TypeScript versions

--- frontend/src/js/components/Documentation/UsingGiantGuide.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import MarkdownPage from "../UtilComponents/MarkdownPage";
-
-export default function UsingGiantGuide() {
-  return <MarkdownPage src="/docs/UsingGiant.md" fullHeight />;
-}
