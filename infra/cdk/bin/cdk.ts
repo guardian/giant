2000f6278ffc8134ff786df49a2e250faa899e95
Tom Richards <tom.richards@guardian.co.uk>
2026-03-16 12:28:53 +0000

    [infra] use generated riff-raff.yaml

--- infra/cdk/bin/cdk.ts
+++ infra/cdk/bin/cdk.ts
@@ -1,18 +1,26 @@
-import 'source-map-support/register';
-import { App } from 'aws-cdk-lib';
 import { Postgres } from '../lib/postgres';
+import {App} from "aws-cdk-lib";
+import {RiffRaffYamlFile} from "@guardian/cdk/lib/riff-raff-yaml-file";
 
 const app = new App();
 
 const stack = 'pfi-giant';
 
+const env = { region: "eu-west-1" };
+
 new Postgres(app, 'pfi-giant-postgres-CODE', {
+	env,
 	stack,
 	stage: 'CODE',
 	app: 'postgres',
 });
 new Postgres(app, 'pfi-giant-postgres-PROD', {
+	env,
 	stack,
 	stage: 'PROD',
 	app: 'postgres',
 });
+
+// synthing the riff-raff.yaml explicitly (rather than replacing 'new App()' with GuRoot) so we can alter deployments
+const riffRaff = new RiffRaffYamlFile(app);
+riffRaff.synth();
