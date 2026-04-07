805b77b03eb638122910a196fb0a64d8394966eb
S Dimmock <sabina.bejasa-dimmock@theguardian.com>
2026-04-07 15:30:09 +0100

    Remove code that used sdkv1 now we're on v2

--- common/src/main/scala/utils/AwsCredentials.scala
+++ common/src/main/scala/utils/AwsCredentials.scala
@@ -1,21 +1,9 @@
 package utils
 
-import com.amazonaws.auth.profile.{ProfileCredentialsProvider => ProfileCredentialsProviderV1}
-import com.amazonaws.auth.{AWSCredentialsProviderChain => AWSCredentialsProviderChainV1}
-import com.amazonaws.auth.{AWSStaticCredentialsProvider => AWSStaticCredentialsProviderV1}
-import com.amazonaws.auth.{BasicAWSCredentials => BasicAWSCredentialsV1}
-import com.amazonaws.auth.{AWSCredentialsProvider => AWSCredentialsProviderV1}
-import com.amazonaws.auth.{InstanceProfileCredentialsProvider => InstanceProfileCredentialsProviderV1}
 import software.amazon.awssdk.auth.credentials.{AwsBasicCredentials, AwsCredentialsProvider, AwsCredentialsProviderChain, InstanceProfileCredentialsProvider, ProfileCredentialsProvider, StaticCredentialsProvider}
 import scala.jdk.CollectionConverters._
 
 object AwsCredentials {
-  def apply(accessKey: Option[String] = None, secretKey: Option[String] = None, profile: Option[String] = None): AWSCredentialsProviderV1 = {
-    // You must never break the chain...
-    // Static credentials should come first in order to override any federated credentials a dev may have on their machine
-    new AWSCredentialsProviderChainV1(minioCredentials(accessKey, secretKey) ++ awsCredentials(profile) : _*)
-  }
-
   def credentialsV2(accessKey: Option[String] = None, secretKey: Option[String] = None, profile: Option[String] = None): AwsCredentialsProvider = {
     val credentialsProviders = minioCredentialsV2(accessKey, secretKey) ++ awsCredentialsV2(profile)
     AwsCredentialsProviderChain.builder()
@@ -23,27 +11,11 @@ object AwsCredentials {
       .build()
   }
 
-  private def awsCredentials(profile: Option[String]): List[AWSCredentialsProviderV1] = {
-    List(
-      new ProfileCredentialsProviderV1(profile.getOrElse("investigations")),
-      InstanceProfileCredentialsProviderV1.getInstance()
-    )
-  }
-
   private def awsCredentialsV2(profile: Option[String]): List[AwsCredentialsProvider] = {
     List(
       ProfileCredentialsProvider.create(profile.getOrElse("investigations")),
       InstanceProfileCredentialsProvider.create()
     )
-  }
-
-  private def minioCredentials(accessKey: Option[String], secretKey: Option[String]): List[AWSCredentialsProviderV1] = {
-    (accessKey, secretKey) match {
-      case (Some(ak), Some(sk)) =>
-        List(new AWSStaticCredentialsProviderV1(new BasicAWSCredentialsV1(ak, sk)))
-      case _ =>
-        List.empty
-    }
   }
 
   private def minioCredentialsV2(accessKey: Option[String], secretKey: Option[String]): List[AwsCredentialsProvider] = {
--- common/src/main/scala/utils/AwsS3Clients.scala
+++ common/src/main/scala/utils/AwsS3Clients.scala
@@ -1,33 +1,15 @@
 package utils
 
 import software.amazon.awssdk.transfer.s3.S3TransferManager
-import software.amazon.awssdk.auth.credentials.{AwsCredentialsProvider => AwsCredentialsProviderV2}
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider
 import software.amazon.awssdk.regions.Region
-import software.amazon.awssdk.services.s3.{S3Client => S3ClientV2}
+import software.amazon.awssdk.services.s3.S3Client
 import software.amazon.awssdk.services.s3.S3AsyncClient
-
-import com.amazonaws.auth.AWSCredentialsProvider
-import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration
-import com.amazonaws.services.s3.{AmazonS3, AmazonS3ClientBuilder}
-import com.amazonaws.services.s3.transfer.{TransferManager, TransferManagerBuilder}
 
 import java.net.URI
 
 object AwsS3Clients {
-  def apply(credentials: AWSCredentialsProvider, region: String, endpoint: Option[String]): (AmazonS3, TransferManager) = {
-    val amazonS3 = buildS3Client(credentials, region, endpoint)
-
-    val transferManagerBuilder = TransferManagerBuilder.standard()
-      .withS3Client(amazonS3)
-      .withMultipartUploadThreshold(100L * 1024 * 1024)
-      .withMinimumUploadPartSize(100L * 1024 * 1024)
-
-    val transferManager = transferManagerBuilder.build()
-
-    amazonS3 -> transferManager
-  }
-
-  def apply(credentials: AwsCredentialsProviderV2, region: Region, endpoint: Option[String]): (S3ClientV2, S3TransferManager) = {
+  def apply(credentials: AwsCredentialsProvider, region: Region, endpoint: Option[String]): (S3Client, S3TransferManager) = {
     val amazonS3 = buildS3ClientV2(credentials, region, endpoint)
     val amazonS3Async = buildS3ClientAsyncV2(credentials, region, endpoint)
 
@@ -39,26 +21,10 @@ object AwsS3Clients {
     amazonS3 -> transferManager
   }
 
-  private def buildS3Client(credentials: AWSCredentialsProvider, region: String, endpoint: Option[String]): AmazonS3 = endpoint match {
+  private def buildS3ClientV2(credentials: AwsCredentialsProvider, region: Region, endpoint: Option[String]): S3Client = endpoint match {
     case Some(minioEndpoint) =>
       // https://docs.minio.io/docs/how-to-use-aws-sdk-for-java-with-minio-server
-      AmazonS3ClientBuilder.standard()
-        .withEndpointConfiguration(new EndpointConfiguration(minioEndpoint, region))
-        .withPathStyleAccessEnabled(true)
-        .withCredentials(credentials)
-        .build()
-
-    case _ =>
-      AmazonS3ClientBuilder.standard()
-        .withCredentials(credentials)
-        .withRegion(region)
-        .build()
-  }
-
-  private def buildS3ClientV2(credentials: AwsCredentialsProviderV2, region: Region, endpoint: Option[String]): S3ClientV2 = endpoint match {
-    case Some(minioEndpoint) =>
-      // https://docs.minio.io/docs/how-to-use-aws-sdk-for-java-with-minio-server
-      S3ClientV2.builder()
+      S3Client.builder()
         .endpointOverride(new URI(minioEndpoint))
         .credentialsProvider(credentials)
         .forcePathStyle(true)
@@ -66,13 +32,13 @@ object AwsS3Clients {
         .build()
 
     case _ =>
-      S3ClientV2.builder()
+      S3Client.builder()
         .credentialsProvider(credentials)
         .region(region)
         .build()
   }
 
-  private def buildS3ClientAsyncV2(credentials: AwsCredentialsProviderV2, region: Region, endpoint: Option[String]): S3AsyncClient = endpoint match {
+  private def buildS3ClientAsyncV2(credentials: AwsCredentialsProvider, region: Region, endpoint: Option[String]): S3AsyncClient = endpoint match {
     case Some(minioEndpoint) =>
       // https://docs.minio.io/docs/how-to-use-aws-sdk-for-java-with-minio-server
       S3AsyncClient.builder()
@@ -89,8 +55,8 @@ object AwsS3Clients {
         .build()
   }
 
-  def pandaS3Client(credentials: AwsCredentialsProviderV2, region: String): S3ClientV2 = {
-    S3ClientV2.builder()
+  def pandaS3Client(credentials: AwsCredentialsProvider, region: String): S3Client= {
+    S3Client.builder()
       .credentialsProvider(credentials)
       .region(Region.of(region))
       .build()
