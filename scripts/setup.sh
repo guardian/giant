9168c5d13d9712c303b16b002b9aad07e70fd0b7
philmcmahon <philip.mcmahon@theguardian.com>
2026-07-16 08:57:40 +0100

    Add migrations to setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 
+echo "Installing contents of Brewfile..."
 brew bundle
+
+echo "Installing frontend dependencies..."
 pushd frontend
 npm install
 popd
+
+echo "Starting docker containers for databases/object storage..."
+./scripts/start-containers.sh
+
+echo "Running postgres migrations..."
+pushd infra/migrate-db
+npm install
+npm run start DEV 8432
+popd
+
+echo "Setup complete"
